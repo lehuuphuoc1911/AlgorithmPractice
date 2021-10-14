4b072bfa09101f91d8bed66573cc1860bebf3a13
Huu Phuoc <lehuuphuocksbk@gmail.com>
2021-10-14 22:30:36 +0900

    stock commit

--- src/com/portfolio/App.java
+++ src/com/portfolio/App.java
@@ -58,3 +58,29 @@ public class App {
         System.out.println(t2-t1);
     }
 }
+/* THIS IS THE RESULT WHICH IS PRINTED ON THE CONSOLE
+* HPG-->  Return :53.807602361948405 Drawdown Average :16.152968661632748
+FPT-->  Return :32.44515520576743 Drawdown Average :7.1358462644125265
+MSN-->  Return :23.345434626154933 Drawdown Average :24.969638741412293
+VIC-->  Return :38.46985029992498 Drawdown Average :9.630242322469885
+com.portfolio.strategy.MinDrawdownStrategy
+search 160000 cases
+Return = 35.31522946568105
+MinDrawdown = 5.547273590824271
+[19, 18, 0, 10]
+--------------------------------------------
+com.portfolio.strategy.MaxReturnOverSTDStrategy
+search 160000 cases
+Return = 31.787280008400327
+MaxReturnOverSTD = 1.1305480261513114
+[0, 17, 0, 4]
+--------------------------------------------
+com.portfolio.strategy.MaxReturnOverDrawDownStrategy
+search 160000 cases
+Return = 37.78498492009014
+maxReturnOverDrawdown = 6.604968421513805
+[13, 5, 0, 4]
+43505
+
+Process finished with exit code 0
+* */
